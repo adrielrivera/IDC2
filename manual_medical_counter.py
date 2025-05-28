9b9e222baa2c84f1e99c5b713eb5aafea89169d0
adrielrivera <adrielrivera103@gmail.com>
2025-05-28 17:11:24 +0800

    please work

--- manual_medical_counter.py
+++ manual_medical_counter.py
@@ -95,11 +95,11 @@ print("Camera initialized.")
 # Initialize Roboflow for Medical Supplies
 print("Initializing Roboflow model for Medical Supplies ('green-bean')...")
 model_medical = None
-ROBOFLOW_MODEL_VERSION_TO_LOAD = 1
+ROBOFLOW_MODEL_VERSION_TO_LOAD = 2
 
 try:
     rf = Roboflow(api_key="q4Y1pRJA0SETfWqL4kKU") 
-    project_medical = rf.workspace().project("green-bean") 
+    project_medical = rf.workspace().project("green-bean-5uqkj") 
     print(f"Attempting to load model version: {ROBOFLOW_MODEL_VERSION_TO_LOAD}")
     model_medical = project_medical.version(ROBOFLOW_MODEL_VERSION_TO_LOAD).model
     
